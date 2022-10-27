5d8a119e0c2ddc91cf0bdc93226a41bfcd75fc48
aflocon <94899000+aflocon@users.noreply.github.com>
2022-10-27 11:02:14 +0200

    Add files via upload

---
+++ tp1_part_1_pi_partial_sum.cpp
@@ -0,0 +1,115 @@
+/*
+
+This program will numerically compute the integral of
+
+                  4/(1+x*x)
+
+from 0 to 1.  The value of this integral is pi -- which
+is great since it gives us an easy way to check the answer.
+
+History: Written by Tim Mattson, 11/1999.
+         Modified/extended by Jonathan Rouzaud-Cornabas, 10/2022
+*/
+
+
+#include <limits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sys/time.h>
+
+#include <omp.h>
+
+#include <iostream>
+#include <ios>
+#include <fstream>
+using namespace std;
+
+static long num_steps = 100000000;
+static int num_core = 4;
+double step;
+
+int main (int argc, char** argv)
+{
+
+      // Read command line arguments.
+      for ( int i = 0; i < argc; i++ ) {
+        if ( ( strcmp( argv[ i ], "-N" ) == 0 ) || ( strcmp( argv[ i ], "-num_steps" ) == 0 ) ) {
+            num_steps = atol( argv[ ++i ] );
+            /*printf( " User num_steps is %ld\n", num_steps );*/
+        } else if ( ( strcmp( argv[ i ], "-h" ) == 0 ) || ( strcmp( argv[ i ], "-help" ) == 0 ) ) {
+            printf( "  Pi Options:\n" );
+            printf( "  -num_steps (-N) <int>:      Number of steps to compute Pi (by default 100000000)\n" );
+            printf( "  -help (-h):            print this message\n\n" );
+            exit( 1 );
+        } else if ( ( strcmp( argv[ i ], "-C" ) == 0 ) || ( strcmp( argv[ i ], "-core" ) == 0 ) ) {
+						num_core = atoi( argv[ ++i ]);
+						/*printf( " User num_core is  %ld\n", num_core );*/
+				}
+      }
+
+	  int i;
+    int j;
+	  double x, pi, sum = 0.0;
+    //int N = 50;
+
+      step = 1.0/(double) num_steps;
+
+      // Timer products.
+      struct timeval begin, end;
+
+      gettimeofday( &begin, NULL );
+
+/**
+		#pragma omp parallel num_threads(num_core)
+	  for ( j=0;  j<= num_core; j++ ){
+      for(i=1; i<= num_steps; i++){
+
+		    x = (i-0.5)*step;
+		    sum[j] = sum[j] + 4.0/(1.0+x*x);
+
+      }
+    }
+**/
+    #pragma omp parallel shared(sum) private(x) num_threads(num_core)
+    {
+      int id= omp_get_thread_num(); //returns the thread number of the calling team
+      int num_threads = omp_get_num_threads(); // returns the number of threads in the current team
+      printf("Thread number %ld  and the number of threads %ld and we choose %ld cores \n ",id,num_threads,num_core);
+
+      int istart = id*num_steps/num_threads;   //N = split array into N parts
+      int iend = (id+1)*num_steps/num_threads;
+      printf("istart %ld  and iend %ld \n ",istart, iend);
+
+      if(id==num_threads-1){
+        iend=num_steps;
+      }
+      for(int i=istart; i<iend; i++){
+        x = (i-0.5)*step;
+
+        #pragma omp atomic
+  		  sum = sum + 4.0/(1.0+x*x);
+      }
+    }
+	  pi = step * sum;
+
+
+      gettimeofday( &end, NULL );
+
+      // Calculate time.
+      double time = 1.0 * ( end.tv_sec - begin.tv_sec ) +
+                1.0e-6 * ( end.tv_usec - begin.tv_usec );
+
+      printf("pi with %ld steps is %lf in %lf seconds with %ld core \n ",num_steps,pi,time,num_core);
+      //printf("\n");
+
+		ofstream fout;
+
+		fout.open("performance_partial_sum.csv",ios_base::app| ios_base::out); //, ios::out|ios::app)
+		//fout << "num_steps" << "," << "time" << endl;
+		fout << num_core << "," << num_steps << "," << time << "," << pi << "\n";
+
+		fout.close();
+
+
+}
